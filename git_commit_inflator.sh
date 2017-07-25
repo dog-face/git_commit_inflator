1b1388c4e387b52ddfead10e4a19eff5d79acd0c
mznco <admin@mznco.net>
2017-07-25 13:41:23 -0400

    LOL

--- git_commit_inflator.sh
+++ git_commit_inflator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
-fortune | cowsay > README.md
+fortune | cowsay > README.txt
 
 git commit -am "LOL"
 git push
